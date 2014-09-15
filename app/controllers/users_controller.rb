c72d6fd11b5e52d922c9205168efa717ef0f50f8
Matthew Williams & Andrew M Westling <student@epicodus.com>
2014-09-15 12:13:50 -0700

    add form partial and users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
 
   def index
-
+    logged_in?
+    redirect_to new_user_path
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sessions[:user_id] = @user.id
+      redirect_to root
+    else
+      render "new"
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 
 end
