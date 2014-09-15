c6572c61fafea8bd90285d4756f7078fc15555e1
Matthew Williams & Andrew M Westling <student@epicodus.com>
2014-09-15 14:02:52 -0700

    add login view/controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password].pop)
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to root_path
     else
       @user = User.new
       @user.errors.add(:email, "or password is invalid.")
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'users#index'
+  get '/login', :to => 'sessions#new'
 
   resources :sessions
 
