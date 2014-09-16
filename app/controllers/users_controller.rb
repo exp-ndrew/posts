d487eb1c68ecac5450347a4e1dc8e53559e14d3c
Matthew Williams & Andrew M Westling <student@epicodus.com>
2014-09-16 15:35:07 -0700

    add user mention feature and name formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.new
-    @posts = @user.posts
+    @posts = Post.all
   end
 
 private
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,25 @@ class User < ActiveRecord::Base
     colors = ["#d9534f","#f0ad4e","#5bc0de","#5cb85c","#428bca"]
     @color = colors[rand(0..4)]
   end
+
+  def email_format_short
+    self.email[0..5] + ".."
+  end
+
+   def email_format_long
+
+    if self.email.length > 15
+      self.email[0..17] + ".."
+    else
+      self.email[0..17]
+    end
+  end
+
+  def email_name
+    if self["@"]
+      self.email.slice(0..(self.email.index('@') - 1))
+    else
+      self.email
+    end
+  end
 end
